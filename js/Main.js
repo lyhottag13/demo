dbc8b17a091a2aa84e1765d6d484eeb8344daeb7
lyhottag13 <leonardo@promoevt.com>
2025-04-15 23:15:06 -0700

    Added Demo Function

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,17 @@
 const line = document.getElementById("line");
-line.setAttribute("x1", document.documentElement.scrollWidth);
-line.setAttribute("y1", 40);
-line.setAttribute("x2", document.documentElement.scrollWidth);
-line.setAttribute("y2", 90);
-line.setAttribute("style", "stroke:red; stroke-width:14");
+const body = document.getElementById("body");
+const lightbulb = document.getElementById("lightbulb");
+const smile = document.getElementById("smile");
+const lightswitch = document.getElementById("switch");
+const BLACK = "rgb(26, 26, 26)";
+
+let lightsOn = true;
+
+lightswitch.addEventListener("pointerdown", () => {
+    
+    lightsOn = !lightsOn;
+    body.style.backgroundColor = (body.style.backgroundColor === BLACK) ? "white" : BLACK;
+    lightbulb.style.filter = (lightbulb.style.filter === "brightness(0)") ? "brightness(1)" : "brightness(0)";
+    smile.style.filter = (smile.style.filter === "brightness(0.05)") ? "brightness(1)" : "brightness(0.05)";
+    lightswitch.style.transform = (lightsOn) ? "rotate(0deg)" : "rotate(180deg)";
+})
