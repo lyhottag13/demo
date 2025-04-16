c86e92b1a686b40b1fe91c808eb0f6d49543edb4
lyhottag13 <leonardo@promoevt.com>
2025-04-15 23:43:20 -0700

    Updated Readability

--- js/Main.js
+++ js/Main.js
@@ -5,13 +5,14 @@ const smile = document.getElementById("smile");
 const lightswitch = document.getElementById("switch");
 const BLACK = "rgb(26, 26, 26)";
 
-let lightsOn = true;
+let isLightOn = true;
 
 lightswitch.addEventListener("pointerdown", () => {
     
-    lightsOn = !lightsOn;
-    body.style.backgroundColor = (body.style.backgroundColor === BLACK) ? "white" : BLACK;
-    lightbulb.style.filter = (lightbulb.style.filter === "brightness(0)") ? "brightness(1)" : "brightness(0)";
-    smile.style.filter = (smile.style.filter === "brightness(0.05)") ? "brightness(1)" : "brightness(0.05)";
-    lightswitch.style.transform = (lightsOn) ? "rotate(0deg)" : "rotate(180deg)";
+    isLightOn = !isLightOn;
+    line.style.filter = (isLightOn) ? "brightness(1)" : "brightness(0)";
+    body.style.backgroundColor = (isLightOn) ? "white" : BLACK;
+    lightbulb.style.filter = (isLightOn) ? "brightness(1)" : "brightness(0)";
+    smile.style.filter = (isLightOn) ? "brightness(1)" : "brightness(0.05)";
+    lightswitch.style.transform = (isLightOn) ? "rotate(0deg)" : "rotate(180deg)";
 })
