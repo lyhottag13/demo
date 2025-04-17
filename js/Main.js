c5cdce46ec14a844f68a8472de2a3aff2f33f8f7
lyhottag13 <leonardo@promoevt.com>
2025-04-17 00:17:02 -0700

    Added Sound and Compressed Images

--- js/Main.js
+++ js/Main.js
@@ -4,15 +4,39 @@ const lightbulb = document.getElementById("lightbulb");
 const smile = document.getElementById("smile");
 const lightswitch = document.getElementById("switch");
 const BLACK = "rgb(26, 26, 26)";
-
+const audio = {};
 let isLightOn = true;
 
+
+function preloadAudio(audioList) {
+    audioList.forEach(element => {
+        audio[element] = new Audio("resources/audio/" + element + ".mp3");
+        audio[element].preload = "auto";
+        audio[element].load();
+    });
+}
+
+preloadAudio(["click", "imnotbutiwishiwas"]);
+
 lightswitch.addEventListener("pointerdown", () => {
-    
+    playSFX("click");
     isLightOn = !isLightOn;
     line.style.filter = (isLightOn) ? "brightness(1)" : "brightness(0)";
     body.style.backgroundColor = (isLightOn) ? "white" : BLACK;
     lightbulb.style.filter = (isLightOn) ? "brightness(1)" : "brightness(0)";
     smile.style.filter = (isLightOn) ? "brightness(1)" : "brightness(0.05)";
     lightswitch.style.transform = (isLightOn) ? "rotate(0deg)" : "rotate(180deg)";
+    if (!isLightOn) {
+        playSFX("imnotbutiwishiwas");
+    } else {
+        stopSFX("imnotbutiwishiwas");
+    }
 })
+
+function playSFX(name) {
+    audio[name].play();
+}
+function stopSFX(name) {
+    audio[name].pause();
+    audio[name].currentTime = 0;
+}
